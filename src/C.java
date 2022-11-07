ef88d1290dee412e9547a61bce4d79237b094e3f
Matthias Jooß <Jooss.Matthias@gmail.com>
2022-11-07 12:26:32 +0100

    init

---
+++ src/C.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class C {
+    static int f(int[] x) {
+        int y = 2;
+        List<Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.get(0).doubleValue();
+
+//create a stack
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+
+
+
+        return x[0] * 2;
+    }
+}
